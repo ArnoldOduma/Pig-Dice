13248f19946e467044f76229c629fbd3c60ee192
Arnold Oduma <arnoldcliff98@gmail.com>
2019-05-13 02:53:44 +0300

    A function to display the game play window on submit. The name fields are to be replaced with the names entered

--- js/script.js
+++ js/script.js
@@ -23,4 +23,22 @@ $(document).ready(function () {
     $("#game-sec").addClass("non-visible");
   });
 
+  // Go to game-play window
+  $(".startGame").submit(function (event) {
+    player_1 = $("#pl-1").val();
+    player_2 = $("#pl-2").val();
+    event.preventDefault();
+    $(".player-name").slideToggle();
+    $(".home").addClass("non-visible");
+    $("#game-sec").addClass("visible");
+    $("#game-sec").removeClass("non-visible");
+    $(".nav-bar").addClass("shrink");
+    $("#inst-cont").hide();
+    $("#pl-1").val("");
+    $("#pl-2").val("");
+    // give the sections the player names
+    $(".pl-1").text(player_1);
+    $(".pl-2").text(player_2);
+    
+  });
 });
